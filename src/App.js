05c98facc779ab8f11bdf959b8cd7278b21fdb65
michael-prye <michael.prye@gmail.com>
2022-05-12 17:55:15 -0400

    Updated homepage

--- src/App.js
+++ src/App.js
@@ -4,26 +4,26 @@ import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage/HomePage";
 
 function App() {
-  const [gameList, setGameList] = useState([]);
+  // const [gameList, setGameList] = useState([]);
 
-  useEffect(() => {
-    getGameList();
-  }, []);
+  // useEffect(() => {
+  //   getGameList();
+  // }, []);
 
-  async function getGameList() {
-    try {
-      const response = await axios.get("https://localhost:7260/api/games/");
-      console.log(response);
-      setGameList(response.data);
-    } catch (error) {
-      console.error(error.message);
-    }
-  }
+  // async function getGameList() {
+  //   try {
+  //     const response = await axios.get("https://localhost:7260/api/games/");
+  //     console.log(response);
+  //     setGameList(response.data);
+  //   } catch (error) {
+  //     console.error(error.message);
+  //   }
+  // }
 
   return (
     <>
       <Routes>
-        <Route path="/" element={<HomePage gameList={gameList} />} />
+        <Route path="/" element={<HomePage />} />
       </Routes>
     </>
   );
--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,30 +1,64 @@
 import React, { useEffect, useState } from "react";
 import GameTable from "../../components/GameTable/GameTable";
 import { useSearchParams } from "react-router-dom";
+import axios from "axios";
 
-const HomePage = ({ gameList }) => {
+
+const HomePage = (props ) => {
   const [searchParams] = useSearchParams();
-  const [filteredGameList, setFilteredGameList] = useState(gameList);
+  const [filteredGameList, setFilteredGameList] = useState(null);
+  const [gameList, setGameList] = useState([]);
+
   const searchTerm =
     searchParams.get("search"); /* localhost:3000/?search="black ops"/ */
 
   useEffect(() => {
     filterGames();
-  }, [gameList, searchTerm]);
+  }, []);
+
+  useEffect(() => {
+    getGameList();
+  }, []);
+
+  async function getGameList() {
+    try {
+      const response = await axios.get("https://localhost:7260/api/games/");
+      console.log(response);
+      setGameList(response.data);
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
 
   function filterGames() {
-    // debugger;
+    //debugger;
     let filteredGames = gameList.filter((game) => {
       if (searchTerm) {
         return game.name === searchTerm;
       }
     });
-    setFilteredGameList(filteredGames);
+    if(filterGames.length === 0){
+      setFilteredGameList(null)
+    }else{
+      setFilteredGameList(filteredGames);
+
+    }
+    
   }
 
   return (
     <>
-      <GameTable filteredGameList={filteredGameList} />
+    {filteredGameList !== null  ?(
+      <GameTable filteredGameList={filteredGameList}/>
+      
+
+    ):(
+      <GameTable filteredGameList={gameList}/>
+    )
+      
+    
+    }
+       
     </>
   );
 };
