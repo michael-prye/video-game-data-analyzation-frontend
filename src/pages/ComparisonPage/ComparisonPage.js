7554ccb6a336eb6ba30f909bb65ee6604ee57f15
Joe <95554565+joerybarczyk@users.noreply.github.com>
2022-05-13 12:33:41 -0400

    Start working on Google Chart on ComparisonPage + Add react-google-charts to project dependencies

--- src/pages/ComparisonPage/ComparisonPage.js
+++ src/pages/ComparisonPage/ComparisonPage.js
@@ -1,34 +1,56 @@
 import React, { useState, useEffect } from "react";
 import { useSearchParams } from "react-router-dom";
+import { Chart } from "react-google-charts";
 
-const ComparisonPage = ({gameList}) => {
+const ComparisonPage = ({ gameList }) => {
   const [searchParams] = useSearchParams();
   const gameName = searchParams.get("name");
   const [filteredGameList, setFilteredGameList] = useState([]);
 
-  useEffect(() =>{
-    filterGames();
-  },[]);
+  const options = {
+    chart: {
+      title: `${gameName}`,
+    },
+  };
 
-  function filterGames(){
+  useEffect(() => {
+    filterGames();
+    generateSalesData();
+  }, []);
+
+  function filterGames() {
     let filteredGames;
     filteredGames = gameList.filter((game) => {
       return game.name === gameName;
-    }
-
-    )
+    });
     setFilteredGameList(filteredGames);
+  }
+
+  function generateSalesData() {
+    const data = [["Platform", "N.A.", "Europe", "Japan", "Global"]];
+    for (const game in filteredGameList) {
+      data.push([
+        game.platform,
+        game.northAmericaSales,
+        game.europeSales,
+        game.japanSales,
+        game.globalSales,
+      ]);
+    }
+    return data;
   }
 
   return (
     <>
       <h1>{gameName}</h1>
 
-      {filteredGameList.map((game) => (
-        <h2>{game.platform}</h2>
-      )
-
-      )}
+      <Chart
+        chartType="Bar"
+        width="100%"
+        height="400px"
+        data={generateSalesData()}
+        options={options}
+      />
     </>
   );
 };
