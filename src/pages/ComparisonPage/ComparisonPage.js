f65c8c0bc9c7282364a887f8626e0f75e96bba4a
michael-prye <michael.prye@gmail.com>
2022-05-13 11:55:52 -0400

    Added filter function to get all items for the searched game

--- src/pages/ComparisonPage/ComparisonPage.js
+++ src/pages/ComparisonPage/ComparisonPage.js
@@ -1,13 +1,34 @@
 import React, { useState, useEffect } from "react";
 import { useSearchParams } from "react-router-dom";
 
-const ComparisonPage = (props) => {
+const ComparisonPage = ({gameList}) => {
   const [searchParams] = useSearchParams();
   const gameName = searchParams.get("name");
+  const [filteredGameList, setFilteredGameList] = useState([]);
+
+  useEffect(() =>{
+    filterGames();
+  },[]);
+
+  function filterGames(){
+    let filteredGames;
+    filteredGames = gameList.filter((game) => {
+      return game.name === gameName;
+    }
+
+    )
+    setFilteredGameList(filteredGames);
+  }
 
   return (
     <>
       <h1>{gameName}</h1>
+
+      {filteredGameList.map((game) => (
+        <h2>{game.platform}</h2>
+      )
+
+      )}
     </>
   );
 };
