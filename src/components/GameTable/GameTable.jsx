e8162de72ab988155b70f9ccf8e28fdcb1302305
michael-prye <michael.prye@gmail.com>
2022-05-16 12:57:16 -0400

    Updated GameTable and added basic styling

--- src/components/GameTable/GameTable.jsx
+++ src/components/GameTable/GameTable.jsx
@@ -1,18 +1,37 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./GameTable.css"
 
 const GameTable = ({ filteredGameList }) => {
   return (
     <>
-      <h1>Game List</h1>
-      {filteredGameList.map((game) => {
+      <h1 className="custon-header">Game List</h1>
+      <table className="custom-table">
+        <tr>
+          <th>ID</th>
+          <th>Title</th>
+          <th>Platform</th>
+          <th>Year</th>
+          <th>Publisher</th>
+        </tr>
+        {filteredGameList.map((game) => {
         return (
-          <p>
-            {game.id} {game.name} ({game.platform}){" "}
-            <Link to={`/compare?name=${game.name}`}>Compare Sales</Link>
-          </p>
-        );
-      })}
+          <tr className="custon-row">
+          <td className="custon-row">{game.id}</td>
+          <td className="custon-row">{game.name}</td>
+          <td className="custon-row">{game.platform}</td>
+          <td className="custon-row">{game.year}</td>
+          <td className="custon-row">{game.publisher}</td>
+          <Link className="compare-sales" to={`/compare?name=${game.name}`}>Compare Sales</Link>
+          </tr>
+          );})}
+          
+          
+            
+             
+      </table>
+          
+   
     </>
   );
 };
