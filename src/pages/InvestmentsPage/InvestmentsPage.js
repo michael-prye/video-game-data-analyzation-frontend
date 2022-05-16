457ee93850b749d78178ddabb24527dc75b0cdf8
Joe <95554565+joerybarczyk@users.noreply.github.com>
2022-05-16 14:38:49 -0400

    Add buttons to InvestmentsPage that toggle each chart into view

--- src/pages/InvestmentsPage/InvestmentsPage.js
+++ src/pages/InvestmentsPage/InvestmentsPage.js
@@ -1,14 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import TopSellingConsolesChart from "../../components/charts/TopSellingConsolesChart";
 import TopIndiePublishersChart from "../../components/charts/TopIndiePublishersChart";
 import TopPublishersByConsoleChart from "../../components/charts/TopPublishersByConsoleChart";
 
 const InvestmentsPage = ({ gameList }) => {
+  const [choice, setChoice] = useState("top consoles");
+
+  let chartChoice;
+  switch (choice) {
+    case "top consoles":
+      chartChoice = <TopSellingConsolesChart gameList={gameList} />;
+      break;
+    case "top indie":
+      chartChoice = <TopIndiePublishersChart gameList={gameList} />;
+      break;
+    case "top publishers":
+      chartChoice = <TopPublishersByConsoleChart gameList={gameList} />;
+      break;
+    default:
+      console.log("Not a valid choice");
+      break;
+  }
+
   return (
     <>
-      <TopSellingConsolesChart gameList={gameList} />
-      <TopIndiePublishersChart gameList={gameList} />
-      <TopPublishersByConsoleChart gameList={gameList} />
+      <h1>Investment Tips</h1>
+      <p>Choose one of the following charts to view data</p>
+      <button onClick={() => setChoice("top consoles")}>
+        Best Selling Consoles Since 2013
+      </button>
+      <button onClick={() => setChoice("top indie")}>
+        Top 10 Indie Publishers
+      </button>
+      <button onClick={() => setChoice("top publishers")}>
+        Top Publishers By Console
+      </button>
+      {chartChoice}
     </>
   );
 };
